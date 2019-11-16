36dbb5cf36ed8c88219753816f890a91fc6c1c1d
Khoa Cyruss <dinhdangkhoa0201@gmail.com>
2019-11-16 12:51:34 +0700

    Đăng xuất thành công

--- src/control/MainControl.java
+++ src/control/MainControl.java
@@ -15,6 +15,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import services.NhanVienServices;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -45,13 +47,13 @@ public class MainControl implements Initializable {
 	@FXML
 	Label lblNgay;
 	private UserPassword userPassword;
-	private NhanVien nhanvien;
+	private NhanVien nhanVien;
 	private Stage stage = new Stage();
 
 	public void setValues(NhanVien nv, UserPassword userPassword) {
 		this.userPassword = userPassword;
-		this.nhanvien = nv;
-		lblTen.setText(this.nhanvien.getHoTen());
+		this.nhanVien = nv;
+		lblTen.setText(this.nhanVien.getHoTen());
 	}
 
 	@Override
@@ -67,7 +69,7 @@ public class MainControl implements Initializable {
 				Services services = new Services();
 				NhanVienServices nhanVienServices = services.getNhanVienServices();
 				try {
-					nhanVienServices.dangXuat(this.nhanvien);
+					nhanVienServices.dangXuat(this.nhanVien);
 					System.exit(0);
 				} catch (RemoteException e1) {
 					// TODO Auto-generated catch block
@@ -80,11 +82,28 @@ public class MainControl implements Initializable {
 			Services services = new Services();
 			NhanVienServices nhanVienServices = services.getNhanVienServices();
 			try {
-				nhanVienServices.dangXuat(this.nhanvien);
+				nhanVienServices.dangXuat(this.nhanVien);
 				System.exit(0);
 			} catch (RemoteException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
+			}
+		}
+		else if(e.getSource() == btnDangXuat) {
+			try {
+				if(alert(AlertType.CONFIRMATION, "Confirm Logout", "Do you want to Logout HKVTravel", null).getResult() == ButtonType.OK) {
+					Node node = (Node) e.getSource();
+					Stage stage = (Stage) node.getScene().getWindow();
+					AnchorPane root = new FXMLLoader(getClass().getResource("/gui/DangNhap.fxml")).load();
+					Services services = new Services();
+					NhanVienServices nhanVienServices = services.getNhanVienServices();
+					nhanVienServices.dangXuat(nhanVien);
+					stage.setScene(new Scene(root));
+					stage.show();
+				}
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
 			}
 		}
 		else if(e.getSource() == btnDoiMatKhau) {
@@ -92,7 +111,7 @@ public class MainControl implements Initializable {
 				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/DoiMatKhau.fxml"));
 				BorderPane paneDoiMatKhau = fxmlLoader.load();
 				DoiMatKhauControl doiMatKhauControl = fxmlLoader.getController();
-				doiMatKhauControl.setValues(nhanvien, userPassword);
+				doiMatKhauControl.setValues(nhanVien, userPassword);
 				border_pane.setCenter(paneDoiMatKhau);
 			} catch (Exception e2) {
 				// TODO: handle exception
@@ -105,7 +124,7 @@ public class MainControl implements Initializable {
 				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/gui/ThongTinCaNhan.fxml"));
 				BorderPane paneThongTinCaNhan = fxmlLoader.load();
 				ThongTinCaNhanControl thongTinCaNhanControl = fxmlLoader.getController();
-				thongTinCaNhanControl.setValues(nhanvien);
+				thongTinCaNhanControl.setValues(nhanVien);
 				border_pane.setCenter(paneThongTinCaNhan);
 			} catch (Exception e2) {
 				// TODO: handle exception
